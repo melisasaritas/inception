8e78a60a212bb056def3dcb40ea3c0dc37472c9f
melisasaritas <melisa.saritas@std.medipol.edu.tr>
2024-09-13 18:00:58 +0300

    donee

--- Inception/srcs/requirements/nginx/tools/script.sh
+++ Inception/srcs/requirements/nginx/tools/script.sh
@@ -2,8 +2,40 @@
 
 if [ ! -f /etc/ssl/certs/nginx.crt ]; then
 echo "Nginx: setting up ssl ...";
-openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=TR/ST=ISTANBUL/L=MASLAK/O=42Istanbul/CN=msaritas.42.fr";
+openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out $CERTS_ -subj "/C=TR/ST=ISTANBUL/L=MASLAK/O=42Istanbul/CN=msaritas.42.fr";
 echo "Nginx: ssl is set up!";
 fi
+
+echo "
+server {
+    listen 443 ssl;
+    listen [::]:443 ssl;
+
+    #server_name $DOMAIN_NAME;
+
+    ssl_certificate $CERTS_;
+    ssl_certificate_key /etc/ssl/private/nginx.key;" > /etc/nginx/sites-available/default
+
+
+echo '
+    ssl_protocols TLSv1.3;
+
+    index index.php;
+    root /var/www/html;
+
+    location / {
+        try_files $uri $uri/ /index.php$is_args$args;
+    }
+
+    location ~ \.php$ {
+        try_files $uri =404;
+        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        fastcgi_pass wordpress:9000;
+        fastcgi_index index.php;
+        include fastcgi_params;
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        fastcgi_param PATH_INFO $fastcgi_path_info;
+    }
+} ' >>  /etc/nginx/sites-available/default
 
 nginx -g "daemon off;"
--- Inception/srcs/requirements/wordpress/tools/script.sh
+++ Inception/srcs/requirements/wordpress/tools/script.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
-mkdir /var/www/
-mkdir /var/www/html
-cd /var/www/html
-rm -rf *
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
-wp core download --allow-root
-mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-mv /wp-config.php /var/www/html/wp-config.php
-curl -s https://api.wordpress.org/secret-key/1.1/salt/ >> /var/www/html/wp-config.php
-echo "if ( ! defined( 'ABSPATH' ) ) {" >> /var/www/html/wp-config.php
-echo "    define( 'ABSPATH', '/var/www/html/' );" >> /var/www/html/wp-config.php
-echo "}" >> /var/www/html/wp-config.php
-echo "" >> /var/www/html/wp-config.php # Add an empty line for readability
-echo "require_once ABSPATH . 'wp-settings.php';" >> /var/www/html/wp-config.php
-sed -i -r "s/db1/$db_name/1"   wp-config.php
-sed -i -r "s/user/$db_user/1"  wp-config.php
-sed -i -r "s/pwd/$db_pwd/1"    wp-config.php
-wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-wp plugin update --all --allow-root
+if [ ! -f /var/www/html/wp-config.php ]; then
+    mkdir /var/www/
+    mkdir /var/www/html
+    cd /var/www/html
+    rm -rf *
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
+    chmod +x wp-cli.phar
+    mv wp-cli.phar /usr/local/bin/wp
+    wp core download --allow-root
+    mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+    mv /wp-config.php /var/www/html/wp-config.php
+    curl -s https://api.wordpress.org/secret-key/1.1/salt/ >> /var/www/html/wp-config.php
+    echo "if ( ! defined( 'ABSPATH' ) ) {" >> /var/www/html/wp-config.php
+    echo "    define( 'ABSPATH', '/var/www/html/' );" >> /var/www/html/wp-config.php
+    echo "}" >> /var/www/html/wp-config.php
+    echo "" >> /var/www/html/wp-config.php # Add an empty line for readability
+    echo "require_once ABSPATH . 'wp-settings.php';" >> /var/www/html/wp-config.php
+    sed -i -r "s/db1/$db_name/1"   wp-config.php
+    sed -i -r "s/user/$db_user/1"  wp-config.php
+    sed -i -r "s/pwd/$db_pwd/1"    wp-config.php
+    wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+    wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+    wp plugin update --all --allow-root
+    mkdir /run/php
+fi
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
-mkdir /run/php
 /usr/sbin/php-fpm7.3 -F
